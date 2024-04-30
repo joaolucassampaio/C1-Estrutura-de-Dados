84c0433324cbd187d92b8b0b85b8f8052e4a8b7e
João Lucas Sampaio <160886470+joaolucassampaio@users.noreply.github.com>
2024-04-30 18:35:43 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> //Biblioteca para
+#include <time.h> //Definição da biblioteca para acesso aos números aleatórios
 
 //Definição da estrutura básica de um nó
 struct node {
@@ -18,11 +18,11 @@ struct descriptor {
 
 int main(int argc, char *argv[]) {
 	struct node *lista, *p, *q, *r; //Definição de ponteiros auxiliares para manipulação dos nós
-	struct descriptor *d; //Definição de um ponteiro auxiliares para manipulação do descritor
+	struct descriptor *d; //Definição de um ponteiro auxiliar para manipulação do descritor
 	int quantifyNode; //Definição de uma variável inteira para armazenar a quantidade de nós a serem inseridos na lista
 	int count = 0; //Definição uma variável inteira para contagem de iterações
 	
-	//d recebe o endereço da memória 
+	//d recebe o endereço da memória onde uma nova instância da estrutura básica de um descritor está sendo alocada dinamicamente
 	d = malloc(sizeof(struct descriptor));
 	//Campos size, first e last são inicializados indicando que a lista está vazia
 	d->size = 0;
@@ -39,28 +39,21 @@ int main(int argc, char *argv[]) {
 	
 	//Looping do-while que executa até que o número desejado de nós seja inserido na lista encadeada
 	do {
-		// É criado um novo nó que recebe um valor aleatório e é colocado no início da fila
+		//É criado um novo nó que recebe um valor aleatório e é colocado no início da fila
 		//p recebe o endereço da memória onde uma nova instância da estrutura básica de um nó está sendo alocada dinamicamente
 		p = malloc(sizeof(struct node));
-		//Inserção no início com novos nós:
+		//Inserção à esquerda:
 		p->info = rand()%99; // Número aleatório de 0 à 98
 		p->next = lista;
 		lista = p;
-		//Aqui a lista já recebe um novo nó
-		//Demonstrar como funciona a inserção à esquerda
 		
 		//Ordenação dos nós
-		// Se a lista estiver vazia ou o valor for menor que o primeiro elemento
-		// Pq d->first é igual a null sendo que a lista já tem um novo nó?
-		//Fazer o d->first apontar =
-		if(d->first == NULL || p->info <= d->first->info) {
-			//Inserção à esquerda
+		if(d->first == NULL || p->info <= d->first->info) { // Se a lista estiver vazia ou o valor for menor que o primeiro elemento
 			p->next = d->first;
 			d->first = p;
 		} else {
 			struct node *current = d->first;
-			// Encontra o ponto de inserção na lista
-			//Inserção à direita
+			//Encontra o ponto de inserção na lista
 			while(current->next != NULL && current->next->info < p->info) {
 				current = current->next;
 			}
@@ -83,7 +76,7 @@ int main(int argc, char *argv[]) {
 	}	
 	printf("\n");
 	
-	// Calcular o valor médio dos nós na lista
+	//Calcular o valor médio dos nós na lista
 	temp = d->first;
     int soma = 0;
     while (temp != NULL) {
@@ -94,7 +87,7 @@ int main(int argc, char *argv[]) {
     printf("\nValor medio:\n");
 	printf("%.2f\n", media);
 	
-	// Remover o primeiro nó com valor imediatamente superior ao valor médio
+	//Remover o primeiro nó com valor imediatamente superior ao valor médio
     struct node *anterior = NULL;
     struct node *atual = d->first;
     while (atual != NULL && atual->info <= media) {
@@ -112,7 +105,7 @@ int main(int argc, char *argv[]) {
     }
     
     printf("\nLista com descritor final:\n");
-    // Imprimir a lista com descritor final
+    //Imprimir a lista com descritor final
     temp = d->first;
     while (temp != NULL) {
         printf("%i ", temp->info);
